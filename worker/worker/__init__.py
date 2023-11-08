40214b196a041c22c72f3e24b4c7fa15f13ce02a
Johan <johan@ftm.nl>
2023-11-08 16:56:20 +0100

    bootstrap worker for indexing

---
+++ worker/worker/__init__.py
@@ -0,0 +1,48 @@
+import os
+import click
+import logging
+import asyncio
+import json
+import psycopg2
+from urllib.parse import urlparse
+
+logging.basicConfig(level=logging.INFO)
+
+conn = psycopg2.connect(os.environ.get("POSTGRES_URI"))
+cursor = conn.cursor()
+cursor.execute("LISTEN page;")
+conn.commit()
+
+
+def elasticsearch_url(path):
+    url = urlparse(os.environ.get("ELASTICSEARCH_URI"))
+    return url._replace(path=path).geturl()
+
+
+def process_page(id, pagestream_id):
+    logging.info(f"{id}")
+
+
+def reader():
+    conn.poll()
+    for notification in conn.connection.notifies:
+        object = json.loads(notification.payload)
+
+        match notification.channel:
+            case "page":
+                process_page(**object)
+
+    conn.notifies.clear()
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def process_messages():
+    logging.info("Processing messages")
+    loop = asyncio.get_event_loop()
+    loop.add_reader(conn.connection, reader)
+    loop.run_forever()
