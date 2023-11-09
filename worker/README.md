9e7cb242daef72b6374bc434adb19fe02c19d224
Johan <johan@ftm.nl>
2023-11-09 13:17:12 +0100

    merge worker and ingest

--- worker/README.md
+++ worker/README.md
@@ -1,5 +1,11 @@
 
-# Worker
+# Ingest
 
-Indexes from Postgres into ElasticSearch
+Take PDF pagestreams and turns them into usable documents.
 
+## Developing
+
+SQLAlchemy models are generated automatically from the database schema:
+```
+make sqlacodegen
+```
--- worker/insight_worker/__init__.py
+++ worker/insight_worker/__init__.py
@@ -1,20 +1,80 @@
-import os
 import click
-import logging
-import asyncio
 import json
+import asyncio
+import os
+import logging
+import ocrmypdf
 import requests
-from sqlalchemy import create_engine, text, select
-from sqlalchemy.orm import Session
 from urllib.parse import urlparse
-from .models import File
+from multiprocessing import Process
+from tempfile import TemporaryDirectory
+from pathlib import Path
+from pikepdf import Pdf
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+from .models import Pagestream, File
 
 logging.basicConfig(level=logging.INFO)
 
 engine = create_engine(os.environ.get("POSTGRES_URI"))
 conn = engine.connect()
-conn.execute(text("listen page;"))
+conn.execute(text("listen pagestream; listen file;"))
 conn.commit()
+
+
+def process_pagestream(id, path, name):
+    logging.info(f"Ingesting pagestream {id}")
+
+    with Pdf.open(path) as pdf:
+        to_page = len(pdf.pages)
+
+    with Session(engine) as session:
+        file = File(name=name, from_page=0, to_page=to_page, pagestream_id=id)
+        session.add(file)
+        session.commit()
+
+
+def ocrmypdf_process(input_file, output_file, pagestream_id, from_page):
+    ocrmypdf.ocr(
+        input_file,
+        output_file,
+        force_ocr=True,
+        language="nld",
+        plugins=["insight_worker.plugin"],
+        pagestream_id=pagestream_id,
+        from_page=from_page,
+    )
+
+
+def extract_file(input_file, from_page, to_page, output_file):
+    destination = Pdf.new()
+    with Pdf.open(input_file) as pdf:
+        for page in pdf.pages[from_page:to_page]:
+            destination.pages.append(page)
+        destination.copy_foreign(pdf.docinfo)
+        destination.save(output_file)
+
+
+def process_file(id, pagestream_id, from_page, to_page, name):
+    logging.info(f"Saving pages {from_page}:{to_page} as file {id}")
+
+    with Session(engine) as session:
+        pagestream = session.query(Pagestream).get(pagestream_id)
+
+    with TemporaryDirectory() as directory:
+        temp_file = Path(directory) / "file.pdf"
+
+        # Extract file from pagestream
+        extract_file(pagestream.path, from_page, to_page, temp_file)
+
+        # OCR & optimize new PDF
+        output_file = Path(os.environ.get("INSIGHT_FILES_PATH")) / f"{id}.pdf"
+        process = Process(
+            target=ocrmypdf_process,
+            args=(temp_file, output_file, pagestream_id, from_page),
+        )
+        process.start()
+        process.join()
 
 
 def elasticsearch_url(path):
@@ -54,8 +114,10 @@ def reader():
         object = json.loads(notification.payload)
 
         match notification.channel:
-            case "page":
-                process_page(**object)
+            case "pagestream":
+                process_pagestream(**object)
+            case "file":
+                process_file(**object)
 
     conn.connection.notifies.clear()
 
